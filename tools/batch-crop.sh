f7a000141d5fc770047b45470bd8b767333f96af
B Horn <b@horn.uk>
2020-04-07 15:00:04 +0100

    update batch crop to use the normal crop script

--- tools/batch-crop.sh
+++ tools/batch-crop.sh
@@ -3,17 +3,12 @@
 # Tool to do batches of crops.
 # takes a list of video files
 
-x_start=240
-y_start=135
-
 while read p; do
     filename=`echo "$p" | cut -d ',' -f 1`
     start=`echo "$p" | cut -d ',' -f 2`
     end=`echo "$p" | cut -d ',' -f 3`
-    t=`basename "$filename"`
-    output=staging/${t%%.*}.mkv
 
-    cat /dev/null | ffmpeg -hwaccel cuda -i "$filename" -vf "crop=w=in_w-$x_start:h=in_h-$y_start:x=$x_start:y=0,scale=1920x1080,setsar=1:1" -c:v h264_nvenc -c:a aac -async 1 -ss "$start" -to "$end" "$output"
+    cat /dev/null | `dirname "$0"`/crop.sh "$filename" "$start" "$end"
 
 done <$1
 
